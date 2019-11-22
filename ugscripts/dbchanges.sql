893b36c4f5346fb250ed4aafaaaa841e8b80c64a
foshdafosh <peter@notjustpcs.co.uk>
2019-11-22 12:57:07 +0000

    Installer repair

--- ugscripts/dbchanges.sql
+++ ugscripts/dbchanges.sql
@@ -3,5 +3,6 @@ CREATE OR REPLACE PROCEDURE pre0c3()
 BEGIN
   UPDATE `oc_setting` SET `value` = 0 WHERE `key` LIKE 'module_%_status';
   UPDATE `oc_setting` SET `value` = 'error.log' WHERE `key` LIKE 'config_error_filename';
+  UPDATE `oc_setting` SET `theme_default_status` = '1' WHERE `code` = 'theme_default';
 END //
 DELIMITER ;
