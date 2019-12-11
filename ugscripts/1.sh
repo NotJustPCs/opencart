51db2b58b6f02b313b65fa3841d019381831e26f
foshdafosh <peter@notjustpcs.co.uk>
2019-12-11 17:02:59 +0000

    Admin redirect and script change

--- ugscripts/1.sh
+++ ugscripts/1.sh
@@ -7,6 +7,9 @@ echo "Remove themes"
 rm -rf /home/gascentre/test.thegascentre.co.uk/catalog/view/theme
 mkdir /home/gascentre/test.thegascentre.co.uk/catalog/view/theme
 
+echo "Remove surplus vqmods"
+rm -rf /home/gascentre/test.thegascentre.co.uk/system/gdpr.ocmod.xml
+
 echo "Make all database changes"
 $db_connect "call pre0c3()"
 
@@ -14,7 +17,7 @@ $db_connect "call pre0c3()"
 echo "Delete the test storage folder"
 rm -rf /home/gascentre/test.storage
 
-echo "Copy OC3 files over the top of the working test site"
+echo "Copy OC3 files from GitHub repo over the top of the working test site"
 rsync -av --exclude-from '/home/gascentre/repositories/opencart-njpc-gc/ugscripts/copyexclusions.txt' /home/gascentre/repositories/opencart-njpc-gc/upload/ /home/gascentre/test.thegascentre.co.uk
 
 echo "Delete the OC3 cache"
